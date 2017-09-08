c6775f266174839e156699e1ef1715b275b3d432
Ramon Martins <ramonmartins.campos@gmail.com>
2017-09-07 21:36:26 -0300

    Melhoria na função piecewise

--- filters.js
+++ filters.js
@@ -10,28 +10,25 @@ photoShop.prototype.blackWhite = function(){
     preview =document.createElement('img');
     preview.src = photo.get();
     canvas.width = preview.width;
-    canvas.height=preview.height;
+    canvas.height = preview.height;
     ctxt = canvas.getContext('2d');
     ctxt.drawImage(preview, 0, 0, preview.width, preview.height);
     var imgData=ctxt.getImageData(0,0,canvas.width,canvas.height);
-    // invert colors
+    // cada um dos 3 pixels de cor recebe o mesmo valor, que é a media entre seus valores originais
     for (var i=0;i<imgData.data.length;i+=4)
       {
-          sum = imgData.data[i] + imgData.data[i+1] + imgData.data[i+2]
-          media = sum/3;
-          
-        imgData.data[i] = media;
-        imgData.data[i+1] = media;
-        imgData.data[i+2] = media;
-        imgData.data[i+3]=255;
+        sum = imgData.data[i] + imgData.data[i+1] + imgData.data[i+2]
+        average = sum/3;  
+        imgData.data[i] = average;
+        imgData.data[i+1] = average;
+        imgData.data[i+2] = average;
       }
       ctxt.putImageData(imgData,0,0);
 }
 photoShop.prototype.threshold = function(constant){
-    if(constant){
-        //ok
-    }else{
-        constant = 383
+    if(!constant){
+        //127 é aproximadamente a metade de 255 e será o limite padrão
+       constant = 127;
     }
     preview =document.createElement('img');
     preview.src = photo.get();
@@ -40,20 +37,18 @@ photoShop.prototype.threshold = function(constant){
     ctxt = canvas.getContext('2d');
     ctxt.drawImage(preview, 0, 0, preview.width, preview.height);
     var imgData=ctxt.getImageData(0,0,canvas.width,canvas.height);
-    // invert colors
+    // dada uma cor, se a intensidade media dela for maior que a constante, então vira preto, caso contrário branco
     for (var i=0;i<imgData.data.length;i+=4)
       {
-          sum = imgData.data[i] + imgData.data[i+1] + imgData.data[i+2]
-          if(sum>constant){
-            imgData.data[i] = 255
-            imgData.data[i+1] = 255
-            imgData.data[i+2] = 255
-          }else{
-            imgData.data[i] = 0
-            imgData.data[i+1] = 0
-            imgData.data[i+2] = 0
-          }
-      imgData.data[i+3]=255;
+        sum = imgData.data[i] + imgData.data[i+1] + imgData.data[i+2];
+        average = sum/3;
+        intensity = 0;
+        if(average > constant) {
+            intensity = 255;
+        }
+        imgData.data[i] = intensity;
+        imgData.data[i+1] = intensity;
+        imgData.data[i+2] = intensity;
       }
       ctxt.putImageData(imgData,0,0);
 }
@@ -70,15 +65,13 @@ photoShop.prototype.negative= function(){
       imgData.data[i]=255-imgData.data[i];
       imgData.data[i+1]=255-imgData.data[i+1];
       imgData.data[i+2]=255-imgData.data[i+2];
-      imgData.data[i+3]=255;
       }
       ctxt.putImageData(imgData,0,0);
 }
 photoShop.prototype.logTransformation= function(constant){
-    if(constant){
-        //ok
-    }else{
-        constant = 1;
+    if(!constant){
+        //analisando a C*ln(x+1), C=45 permite q C*(255+1) = 255
+        constant = 45;
     }
     preview =document.createElement('img');
     preview.src = photo.get();
@@ -88,19 +81,15 @@ photoShop.prototype.logTransformation= function(constant){
     ctxt.drawImage(preview, 0, 0, preview.width, preview.height);
     var imgData=ctxt.getImageData(0,0,canvas.width,canvas.height);
     for (var i=0;i<imgData.data.length;i+=4){
-        imgData.data[i]=constant*(Math.log(1 + (imgData.data[i])/255))*255;
-        imgData.data[i+1]=constant*(Math.log(1 + (imgData.data[i+1])/255))*255;
-        imgData.data[i+2]=constant*(Math.log(1 + (imgData.data[i+2])/255))*255;
-        imgData.data[i+3]=255;
+        imgData.data[i] = constant * (Math.log(imgData.data[i] + 1));
+        imgData.data[i+1] = constant * (Math.log(imgData.data[i+1] + 1));
+        imgData.data[i+2] = constant * (Math.log(imgData.data[i+2] + 1));
     }
 
-        ctxt.putImageData(imgData,0,0);
-    Math.log(2);
+    ctxt.putImageData(imgData,0,0);
 }
 photoShop.prototype.gamma = function (constant,gamma){
-    if(constant){
-        //ok        
-    }else{
+    if(!constant){
         constant = 1;
     }
     preview =document.createElement('img');
@@ -115,7 +104,6 @@ photoShop.prototype.gamma = function (constant,gamma){
         imgData.data[i]=constant*(Math.pow(imgData.data[i]/255,gamma))*255;
         imgData.data[i+1]=constant*(Math.pow(imgData.data[i+1]/255,gamma))*255;
         imgData.data[i+2]=constant*(Math.pow(imgData.data[i+2]/255,gamma))*255;
-        imgData.data[i+3]=255;
         }
 
         ctxt.putImageData(imgData,0,0);
@@ -123,6 +111,7 @@ photoShop.prototype.gamma = function (constant,gamma){
 }
 photoShop.prototype.layer= function (layer){
     layer = layer - 1
+    layer = 7 - layer
     preview =document.createElement('img');
     preview.src = photo.get();
     canvas.width = preview.width;
@@ -131,22 +120,91 @@ photoShop.prototype.layer= function (layer){
     ctxt.drawImage(preview, 0, 0, preview.width, preview.height);
     var imgData=ctxt.getImageData(0,0,canvas.width,canvas.height);
     for (var i=0;i<imgData.data.length;i+=4){
-        var listBin = new String(imgData.data[i].toString(2));
+        var listBin = "";
+
+        listBin = new String(imgData.data[i].toString(2));
         listBin.bitSlicingLayer(layer) 
         num = parseInt(listBin.value,2);
         imgData.data[i]=num;
-        var listBin = new String(imgData.data[i+1].toString(2));
+
+        listBin = new String(imgData.data[i+1].toString(2));
         listBin.bitSlicingLayer(layer) 
         num = parseInt(listBin.value,2);
         imgData.data[i+1]=num;
-        var listBin = new String(imgData.data[i+2].toString(2));
+
+        listBin = new String(imgData.data[i+2].toString(2));
         listBin.bitSlicingLayer(layer) 
         num = parseInt(listBin.value,2);
         imgData.data[i+2]=num;
-        imgData.data[i+3]=255;
+        
     }
 
      ctxt.putImageData(imgData,0,0);
+    
+}
+photoShop.prototype.piecewise= function (points){
+    if(points){
+        if(points.length>=10){
+            //(a,b),(c,d)
+            var a = parseInt(points.split(',')[0].split('(')[1],10);
+            var b =  parseInt(points.split(',')[1].split(')')[0],10);
+            var c =  parseInt(points.split(',')[2].split('(')[1],10);
+            var d =  parseInt(points.split(',')[3].split(')')[0],10);
+            if( a >= 0 && a <= 255 && b >= 0 && b <= 255 && c >= 0 && c <= 255 && d >= 0 && d <= 255 && a <= c){
+                preview =document.createElement('img');
+                preview.src = photo.get();
+                canvas.width = preview.width;
+                canvas.height=preview.height;
+                ctxt = canvas.getContext('2d');
+                ctxt.drawImage(preview, 0, 0, preview.width, preview.height);
+                var imgData=ctxt.getImageData(0,0,canvas.width,canvas.height);
+                for (var i=0;i<imgData.data.length;i+=4){
+                    sum = imgData.data[i] + imgData.data[i+1] + imgData.data[i+2];
+                    average = sum/3;
+                    
+                    intensity = 0;
+                    if(average <= a) {
+
+                        //dois pontos: (0,0) e (a,b)
+                        //m é o coeficiente angular
+                        m = (0 - b)/(0 - a);
+                        //equação da reta y - y1 = m(x - x1)
+                        //sendo (x1,y1) = (0,0)
+                        intensity = m * average;
+                    }else if(average > a && average <= c) {
+
+                        //dois pontos: (a,b) e (c,d)
+                        //m é o coeficiente angular
+                        m = (b - d)/(a - c);
+                        //equação da reta y - y1 = m(x - x1)
+                        //sendo (x1,y1) = (a,b)
+                        intensity = (m * (average - a)) + b;
+                    }else{
+                        //dois pontos: (c,d) e (255,255)
+                        //m é o coeficiente angular
+                        m = (d - 255)/(c - 255);
+                        //equação da reta y - y1 = m(x - x1)
+                        //sendo (x1,y1) = (c,d)
+                        intensity = (m * (average - c)) + d;
+                    }     
+                    imgData.data[i] = intensity;
+                    imgData.data[i+1] = intensity;
+                    imgData.data[i+2] = intensity;
+                    
+                }
+            
+                 ctxt.putImageData(imgData,0,0);
+
+            }else{
+                return;
+            }
+        }else{
+            return;
+        }
+    }else{
+        return;
+    }
+    
     
 }
 var photo = new photoShop();
--- string.js
+++ string.js
@@ -1,8 +1,10 @@
 function String(string) {
     if(string.length<8){
-        keep = string
-        string = new Array(8 - string.length).join( "0" );
-        string = string + keep
+        str = "";
+        for (var i = string.length; i < 8; i++) {
+            str = str + "0";
+        }
+        string = str + string;
     }
     this.value = string
 }
@@ -10,8 +12,9 @@ String.prototype.replaceAt=function(index, replacement) {
     this.value =  this.value.substr(0, this.value.length - index-1) + replacement+ this.value.substr(this.value.length - index-1 + replacement.length);
 }
 String.prototype.bitSlicingLayer=function(index) {
-    var keepValue = this.value[index]
-    var first = new Array(this.value.length - index).join( "0" );
-    var last = new Array( index + 1).join( "0" );
-    this.value = first + keepValue + last
+    if (this.value.charAt(index) === "1") {
+        this.value = "11111111";
+    } else {
+        this.value = "00000000";
+    }
 }
