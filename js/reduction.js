cc195f524363cac530f796f3ed3595c7552704a0
yuribtt <yuribitten@gmail.com>
2017-10-09 22:43:39 -0300

    redução funcionando
    agora está melhor

--- js/reduction.js
+++ js/reduction.js
@@ -8,33 +8,56 @@ reduction.prototype.set = function(preview){
     this._preview = preview;
 }
 
-reduction.prototype.imageReduction = function(mean=false) {
+reduction.prototype.imageReduction = function(tax, mean=false) {
 	preview = reduct.getPreview();
     ctxt = canvas.getContext('2d');
     ctxt.drawImage(reduct.getPreview(), 0 , 0,preview.width, preview.height );
     var imgData = ctxt.getImageData(0, 0, preview.width, preview.height);
-    var new_imgData = ctxt.createImageData(Math.floor(preview.width/2), Math.floor(preview.height/2));
+    var new_imgData = ctxt.createImageData(Math.floor(preview.width/tax), Math.floor(preview.height/tax));
+    //var new_imgData = ctxt.createImageData(preview.width, preview.height);
 
     if (mean) {
     	var m = [[1,1,1],[1,1,1],[1,1,1]];
-    	//var conv = new convolution();
     	var data = conv.applyConv(preview, m, imgData.data, 1/9);
     } else {
     	var data = imgData.data;
     }
 
     var auxPos = 0;
-    for(var i=0; i<preview.height; i+=2) {
-        for(var j=0; j<preview.width; j+=2) {
+    for(var i=0; i<preview.height; i+=tax) {
+        for(var j=0; j<preview.width; j+=tax) {
     		var pos = ((i * preview.width) + j) * 4;
-    		new_imgData.data[auxPos++] = data[pos];
-    		new_imgData.data[auxPos++] = data[pos+1];
-    		new_imgData.data[auxPos++] = data[pos+2];
+    		var intensity = data[pos];
+    		for(k=0; k<3; k++)
+    			new_imgData.data[auxPos++] = intensity;
     		new_imgData.data[auxPos++] = data[pos+3];
         }
     }
 
-   
+    /*for(var i=0; i<preview.height; i+=2) {
+        for(var j=0; j<preview.width; j+=2) {
+    		var pos = ((i * preview.width) + j) * 4;
+    		intensity = data[pos];
+    		for(k=0; k<4; k++)
+    			new_imgData.data[pos+k] = data[pos+k];
+    		if(i<preview.height-1){
+    			pos = (((i+1) * preview.width) + j) * 4;
+    			for(k=0; k<4; k++)
+    				new_imgData.data[pos+k] = data[pos+k];
+    		}
+    		if(j<preview.width-1){
+    			pos = ((i * preview.width) + (j+1)) * 4;
+    			for(k=0; k<4; k++)
+    				new_imgData.data[pos+k] = data[pos+k];
+    		}
+    		if(i<preview.height-1 && j<preview.width-1){
+    			pos = (((i+1) * preview.width) + (j+1)) * 4;
+    			for(k=0; k<4; k++)
+    				new_imgData.data[pos+k] = data[pos+k];
+    		}
+        }
+    }*/
+  
     ctxt.clearRect(0, 0, preview.width, preview.height);
     ctxt.putImageData(new_imgData,0,0);
 }
