6351e419f0deae27d73f6fc5bd68f96573581b66
yuri <yuri@MacBook-Air-de-deway.local>
2017-12-05 10:30:51 -0300

    lzw e huffman mas precisa testar

--- js/compressions.js
+++ js/compressions.js
@@ -213,6 +213,94 @@ compressions.prototype.lzw_decode = function (data) {
     return sequencia
 }
 
+compressions.prototype.huffman_lzw = function () {
+    preview = comp.getPreview();
+    ctxt = canvas.getContext('2d');
+    ctxt.drawImage(comp.getPreview(), 0, 0, preview.width, preview.height);
+    var imgData = ctxt.getImageData(0, 0, preview.width, preview.height);
+
+    var r = [];
+    var g = [];
+    var b = [];
+    for (var i = 0, j = 0; i < imgData.data.length; i += 4, j += 1) {
+        r[j] = (imgData.data[i]);
+        g[j] = (imgData.data[i + 1]);
+        b[j] = (imgData.data[i + 2]);
+    }
+
+    r_seq = comp.lzw_encode(r)
+    g_seq = comp.lzw_encode(g)
+    b_seq = comp.lzw_encode(b)
+
+    var huffmanR = Huffman.treeFromImage(r_seq);
+    var huffmanG = Huffman.treeFromImage(g_seq);
+    var huffmanB = Huffman.treeFromImage(b_seq);
+    var huffmanRencode = huffmanR.encode(r_seq);
+    var huffmanGencode = huffmanG.encode(g_seq);
+    var huffmanBencode = huffmanB.encode(b_seq);
+    var huffmanRTreeEncode = huffmanR.encodeTree();
+    var huffmanGTreeEncode = huffmanG.encodeTree();
+    var huffmanBTreeEncode = huffmanB.encodeTree();
+    var file = Huffman.parseDataToUint8array(preview.width,
+        preview.height,
+        huffmanRTreeEncode,
+        huffmanGTreeEncode,
+        huffmanBTreeEncode,
+        huffmanRencode,
+        huffmanGencode,
+        huffmanBencode);
+
+    var data = new Blob([file]);
+    var a2 = document.getElementById("a2");
+    a2.href = URL.createObjectURL(data);
+
+    ctxt.putImageData(imgData, 0, 0);
+}
+
+compressions.prototype.fileHuffmanLzw = function () {
+    file = document.getElementById("huffman").files[0]; //sames as here
+    var reader = new FileReader();
+    reader.readAsArrayBuffer(file); //reads the data as a txt
+    reader.onloadend = function () {
+
+        file = (reader.result);
+        uint = new Uint8Array(file);
+        var image = Huffman.parseUint8arrayToData(uint);
+
+
+        preview = document.createElement('img');
+        preview.width = image.width;
+        preview.height = image.height;
+
+        canvas.width = preview.width;
+        canvas.height = preview.height;
+        ctxt = canvas.getContext('2d');
+        ctxt.drawImage(preview, canvas.width, 0, canvas.width, canvas.height);
+        var imgData = ctxt.getImageData(0, 0, preview.width, preview.height);
+
+        var huffmanR = Huffman.Tree.decodeTree(JSON.parse(image.huffmanRTree));
+        var huffmanG = Huffman.Tree.decodeTree(JSON.parse(image.huffmanGTree));
+        var huffmanB = Huffman.Tree.decodeTree(JSON.parse(image.huffmanBTree));
+        var huffmanRdecode = huffmanR.decode(image.huffmanREncode);
+        var huffmanGdecode = huffmanG.decode(image.huffmanGEncode);
+        var huffmanBdecode = huffmanB.decode(image.huffmanBEncode);
+
+        var lzwRdecode = comp.lzw_decode(huffmanRdecode)
+        var lzwGdecode = comp.lzw_decode(huffmanGdecode)
+        var lzwBdecode = comp.lzw_decode(huffmanBdecode)   
+
+
+        for (var i = 0, j = 0; i < imgData.data.length; i += 4, j += 1) {
+            imgData.data[i] = lzwRdecode[j];
+            imgData.data[i + 1] = lzwGdecode[j];
+            imgData.data[i + 2] = lzwBdecode[j];
+            imgData.data[i + 3] = 255;
+        }
+        ctxt.putImageData(imgData, 0, 0);
+    }
+
+}
+
 var comp = new compressions();
 
 
